231a478570b8d3ea0f2b9a63789948e0e8373c53
Rukshan <welikadagerrukshanbotheju2002@gmail.com>
2026-03-09 19:49:52 +0530

    Add Contact page

--- components/footer.tsx
+++ components/footer.tsx
@@ -26,8 +26,8 @@ const Footer: React.FC = () => {
         </nav>
         <nav>
           <h6 className="footer-title">Company</h6>
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
+          <Link href="/Aboutus" className="link link-hover">Aboutus</Link>
+         <Link href="/Contact" className="link link-hover">Contact</Link>
          <Link href="/Jobs" className="link link-hover">Jobs</Link>
           <Link href="/Presskit" className="link link-hover">Presskit</Link>
         </nav>
